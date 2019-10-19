52c5ddfd3b9409a209d87e40373c48a2fc2e2594
Scott <scott.chaplinski@gmail.com>
2019-10-18 19:02:01 -0500

    create profile activity complete

---
+++ app/src/main/java/com/example/inspirationrewards/CreateProfile.java
@@ -0,0 +1,23 @@
+package com.example.inspirationrewards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class CreateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.arrow_with_logo);// set drawable icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.create_profile_menu, menu);
+        return true;
+    }
+}
